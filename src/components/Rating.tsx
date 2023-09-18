da3a37f83a2e7038ca7924adf79fcb809c22e443
Andre Grossi <iamandregrossi@gmail.com>
2023-09-17 21:06:08 -0300

    feat(components): Create Rating badge component

---
+++ src/components/Rating.tsx
@@ -0,0 +1,28 @@
+import { styled } from "@/styled-system/jsx";
+
+const Box = styled("div", { base: { borderRadius: "md", px: 2, py: 1 } });
+const Value = styled("span", {
+  base: { color: "slate.900", fontWeight: "semibold" },
+});
+
+export default function Rating({ value }: { value: number }) {
+  if (value <= 40)
+    return (
+      <Box bg="red.400">
+        <Value>{value}</Value>
+      </Box>
+    );
+
+  if (value <= 80)
+    return (
+      <Box bg="yellow.400">
+        <Value>{value}</Value>
+      </Box>
+    );
+
+  return (
+    <Box bg="green.400">
+      <Value>{value}</Value>
+    </Box>
+  );
+}
