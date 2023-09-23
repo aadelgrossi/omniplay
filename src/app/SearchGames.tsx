a6fca235c345f651b14d802e8e04613f66bb2ebe
Andre Grossi <iamandregrossi@gmail.com>
2023-09-23 19:30:05 -0300

    feat(SearchGames): Control query variables via search params

--- src/app/SearchGames.tsx
+++ src/app/SearchGames.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter, useSearchParams } from "next/navigation";
 import { list } from "radash";
 import { useEffect, useState } from "react";
 import { useDebounce } from "react-use";
@@ -37,14 +38,35 @@ function ListGames({ results = [], isLoading }: ListGamesProps) {
 }
 
 export default function SearchGames() {
-  const [inputValue, setInputValue] = useState<string | undefined>();
-  const [search, setSearch] = useState<string | undefined>();
+  const router = useRouter();
+  const params = useSearchParams();
+  const searchParams = new URLSearchParams(params);
 
-  useDebounce(() => setSearch(inputValue), 600, [inputValue]);
+  const search = params.get("q") as string;
+  const page = (params.get("page") || 1) as number;
+  const pageSize = (params.get("page_size") || 10) as number;
 
-  const [pageSize, setPageSize] = useState(10);
   const [totalPages, setTotalPages] = useState(1);
-  const [page, setPage] = useState(1);
+  const [inputValue, setInputValue] = useState(search);
+
+  useDebounce(
+    () => {
+      if (!inputValue) {
+        searchParams.delete("q");
+        router.push(`/?${searchParams.toString()}`);
+        return;
+      }
+      searchParams.set("q", inputValue);
+      router.push(`/?${searchParams.toString()}`);
+    },
+    600,
+    [inputValue]
+  );
+
+  const onChangePageSize = (value: string) => {
+    searchParams.set("page_size", value);
+    router.push(`/?${searchParams.toString()}`);
+  };
 
   const { data, isLoading, isFetching, isFetched } = useGetGames({
     search,
@@ -60,7 +82,8 @@ export default function SearchGames() {
 
   useEffect(() => {
     if (search) {
-      setPage(1);
+      searchParams.set("page", "1");
+      router.push(`/?${searchParams.toString()}`);
     }
   }, [search]);
 
@@ -74,12 +97,18 @@ export default function SearchGames() {
         <Pagination
           totalPages={totalPages}
           page={page}
-          next={() => setPage((page) => page + 1)}
-          prev={() => setPage((page) => page - 1)}
+          next={() => {
+            searchParams.set("page", String(Number(page) + 1));
+            router.push(`/?${searchParams.toString()}`);
+          }}
+          prev={() => {
+            searchParams.set("page", String(Number(page) - 1));
+            router.push(`/?${searchParams.toString()}`);
+          }}
         />
         <Select
           value={pageSize.toString()}
-          onValueChange={(value) => setPageSize(Number(value))}
+          onValueChange={onChangePageSize}
           options={[
             { label: "Show 10 items", value: 10 },
             { label: "Show 20 items", value: 20 },
