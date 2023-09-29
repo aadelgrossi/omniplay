28c9546822b12d5202c4df026be46f83b4d4c737
Andre Grossi <iamandregrossi@gmail.com>
2023-09-29 15:19:25 -0300

    docs: Add GameCard stories

---
+++ src/stories/GameCard.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import GameCard from "@/components/GameCard";
+
+import { styled } from "../../styled-system/jsx";
+
+const meta: Meta<typeof GameCard> = {
+  title: "Components/GameCard",
+  component: GameCard,
+  parameters: {
+    layout: "centered",
+  },
+  decorators: [
+    (Story) => (
+      <styled.div maxWidth="300px">
+        <Story />
+      </styled.div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    game: {
+      id: 2943,
+      name: "God of War 2018",
+      slug: "god-of-war-2",
+      metacritic: 90,
+      parent_platforms: [
+        {
+          platform: {
+            id: 3,
+            slug: "playstation",
+            name: "Playstation",
+          },
+        },
+      ],
+      genres: [
+        {
+          id: 20,
+          slug: "action",
+          name: "Action",
+        },
+      ],
+      background_image:
+        "https://media.rawg.io/media/resize/1920/-/screenshots/928/928cdaf4ae204f202d177bbd65e911b3.jpeg",
+    },
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    isLoading: true,
+  },
+};
