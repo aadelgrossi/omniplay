4f6bbc86058d757d9a2e324e9c0833ef0c85d996
Andre Grossi <iamandregrossi@gmail.com>
2023-09-30 11:33:11 -0300

    fix(Rating): Add padding to fix rating width

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,6 +1,6 @@
 import { Center, styled } from "../../styled-system/jsx";
 
-const Box = styled(Center, { base: { borderRadius: "md", w: 9, h: 9 } });
+const Box = styled(Center, { base: { borderRadius: "md", p: 2, w: 9, h: 9 } });
 const Value = styled("span", {
   base: {
     color: "slate.900",
@@ -12,22 +12,14 @@ const Value = styled("span", {
 export default function Rating({ value }: { value?: number }) {
   if (!value) return <></>;
 
-  if (value <= 40)
-    return (
-      <Box bg="red.400">
-        <Value>{value}</Value>
-      </Box>
-    );
-
-  if (value <= 80)
-    return (
-      <Box bg="yellow.400">
-        <Value>{value}</Value>
-      </Box>
-    );
+  const rating = () => {
+    if (value <= 40) return "red.400";
+    if (value <= 80) return "yellow.400";
+    return "green.400";
+  };
 
   return (
-    <Box bg="green.400">
+    <Box bg={rating()}>
       <Value>{value}</Value>
     </Box>
   );
